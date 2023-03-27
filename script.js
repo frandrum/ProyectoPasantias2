2dffb925c378c8b39c3e656260eb167308f5ee1b
frandrum <100533830+frandrum@users.noreply.github.com>
2023-03-27 06:48:57 -0400

    intentando parte 5

---
+++ script.js
@@ -0,0 +1,6 @@
+function concatenar() {
+    const numero = document.getElementById("numero").value;
+    const url = `https://www.farmatodo.com.ve/producto/${numero}`;
+    window.open(url);
+    document.getElementById("numero").value = "";
+  }
