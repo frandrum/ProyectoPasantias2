7d5fb11f4b5e1395edb20aeb7452a52963a71e29
frandrum <fran.ula314@gmail.com>
2023-03-27 00:48:33 -0400

    No tiene importancia 4.0

--- script.js
+++
@@ -1,7 +0,0 @@
-function concatenar() {
-    const numero = document.getElementById("numero").value;
-    const url = `https://www.farmatodo.com.ve/producto/${numero}`;
-    window.open(url);
-    document.getElementById("numero").value = "";
-  }
-  
