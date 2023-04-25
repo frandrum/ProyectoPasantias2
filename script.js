37d7580802a5a43f18b8620d9a3664d191f20b23
frandrum <fran.ula314@gmail.com>
2023-04-25 04:06:01 -0400

    probando el script mas sencillo

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 function concatenar() {
-  const input = document.getElementById("numero");
-  const numero = input.value.trim().replace(/\s+/g, ''); // elimina espacios en blanco
-  input.value = numero; // actualiza el valor del input
-
-  // verifica que el número solo contenga dígitos
-  const regex = /^\d+$/;
-  if (!regex.test(numero)) {
-    alert("Ingrese solo números pegados sin espacios.");
-    return;
+    const numero = document.getElementById("numero").value;
+    const url = `https://www.farmatodo.com.ve/producto/${numero}`;
+    window.open(url);
+    document.getElementById("numero").value = "";
   }
-
-  const url = `https://www.farmatodo.com.ve/producto/${numero}`;
-  window.open(url);
-  input.value = "";
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  const input = document.getElementById("numero");
-  input.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      const touchEvent = new TouchEvent("touchstart");
-      document.querySelector(".boton").dispatchEvent(touchEvent);
-    }
-  });
-});
