f12711fd2088fcaa35b6a6393c417690b083f6cb
frandrum <fran.ula314@gmail.com>
2023-04-25 03:50:15 -0400

    probando para que con enter se ejecute

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
 function concatenar() {
-    const numero = document.getElementById("numero").value;
-    const url = `https://www.farmatodo.com.ve/producto/${numero}`;
-    window.open(url);
-    document.getElementById("numero").value = "";
-  }
-  function concatenar() {
-    const input = document.getElementById("numero");
-    const numero = input.value.trim().replace(/\s+/g, ''); // elimina espacios en blanco
-    input.value = numero; // actualiza el valor del input
-    
-    // verifica que el número solo contenga dígitos
-    const regex = /^\d+$/;
-    if (!regex.test(numero)) {
-      alert("Ingrese solo números pegados sin espacios.");
-      return;
-    }
-    
-    const url = `https://www.farmatodo.com.ve/producto/${numero}`;
-    window.open(url);
-    input.value = "";
-  }
-  
-  // verifica que solo se ingresen números y no espacios en blanco
   const input = document.getElementById("numero");
-  let timeoutId;
-  
-  input.addEventListener("input", function (event) {
-    clearTimeout(timeoutId);
-    const value = event.target.value;
-    const newValue = value.replace(/\s+/g, '');
-    if (newValue !== value) {
-      event.target.value = newValue;
-    }
-    timeoutId = setTimeout(function () {
-      // desactivar el micrófono aquí
-    }, 2000);
-  });
-  
+  const numero = input.value.trim().replace(/\s+/g, ''); // elimina espacios en blanco
+  input.value = numero; // actualiza el valor del input
+
+  // verifica que el número solo contenga dígitos
+  const regex = /^\d+$/;
+  if (!regex.test(numero)) {
+    alert("Ingrese solo números pegados sin espacios.");
+    return;
+  }
+
+  const url = `https://www.farmatodo.com.ve/producto/${numero}`;
+  window.open(url);
+  input.value = "";
+}
+
+const input = document.getElementById("numero");
+input.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    concatenar();
+  }
+});
